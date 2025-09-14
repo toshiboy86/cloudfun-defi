d13f2e607e5baf20aba52209bb91bd951b9975cd
toshiboy86 <toshibowie86@gmail.com>
2025-09-15 02:10:52 +0900

    Add hero image and update HeroSection component for background display; modify SharedHeader icon viewBox

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -1,29 +1,16 @@
 export function HeroSection() {
   return (
-    <div className="relative h-96 bg-gradient-to-br from-orange-400 via-pink-500 to-orange-600 overflow-hidden">
-      {/* Background Image Elements */}
-      <div className="absolute inset-0">
-        {/* Palm trees silhouette */}
-        <div className="absolute bottom-0 left-10 w-20 h-32 bg-black opacity-20 rounded-t-full"></div>
-        <div className="absolute bottom-0 left-16 w-16 h-28 bg-black opacity-20 rounded-t-full"></div>
-        <div className="absolute bottom-0 right-20 w-18 h-30 bg-black opacity-20 rounded-t-full"></div>
-
-        {/* Water silhouette */}
-        <div className="absolute bottom-0 left-0 right-0 h-16 bg-gradient-to-t from-blue-800 to-transparent opacity-30"></div>
-
-        {/* Mountains silhouette */}
-        <div className="absolute bottom-16 left-0 right-0 h-20 bg-gradient-to-t from-gray-800 to-transparent opacity-40"></div>
-
-        {/* Beach furniture silhouettes */}
-        <div className="absolute bottom-8 right-32 w-8 h-8 bg-black opacity-20 rounded-full"></div>
-        <div className="absolute bottom-6 right-28 w-6 h-6 bg-black opacity-20"></div>
-        <div className="absolute bottom-6 right-36 w-6 h-6 bg-black opacity-20"></div>
-
-        {/* Camel silhouette */}
-        <div className="absolute bottom-12 right-48 w-12 h-8 bg-black opacity-20 rounded-full"></div>
-        <div className="absolute bottom-10 right-46 w-2 h-6 bg-black opacity-20"></div>
-        <div className="absolute bottom-10 right-50 w-2 h-6 bg-black opacity-20"></div>
-      </div>
+    <div className="relative h-96 overflow-hidden">
+      {/* Background Image */}
+      <div 
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+        style={{
+          backgroundImage: 'url(/hero.jpg)',
+        }}
+      />
+      
+      {/* Overlay for better text readability */}
+      <div className="absolute inset-0 bg-black/30"></div>
 
       {/* Content */}
       <div className="relative z-10 flex flex-col items-center justify-center h-full px-6 text-center">
